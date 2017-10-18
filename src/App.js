3388f9443239cafe38158760f5eaf36db88b4f9d
sunhan <583013043@qq.com>
2017-10-18 23:49:55 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by vito on 2017/10/18.
+ */
+
+import React from 'react';
+import { Block } from 'jsxstyle';
+
+export default () => (
+  <Block>Home</Block>
+)
